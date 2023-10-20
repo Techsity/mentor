507d9af673f6004e6772b9de02fd4c055702ff54
Josh-Savvy <joshsavyy007@gmail.com>
2023-10-20 04:07:04 +0100

    feat: mentee ongoing course page

--- components/templates/mentor/details/index.tsx
+++ components/templates/mentor/details/index.tsx
@@ -8,9 +8,6 @@ import {
 	Projects,
 	Skills,
 } from "../../../ui/organisms/mentor/details/";
-import Carousel from "react-multi-carousel";
-import DisplayCourseCard from "../../../ui/atom/common/course/DisplayCourseCard";
-import courses from "../../../../data/courses";
 import { getMentorCourses } from "../../../../services/api";
 import NewsLetterForm from "../../../ui/atom/forms/NewsLetterForm";
 import { AnimationOnScroll } from "react-animation-on-scroll";
