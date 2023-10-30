314b21f55d4afb5f0c6b6748b4fd364cb43d82b3
Josh-Savvy <joshsavyy007@gmail.com>
2023-10-30 15:25:05 +0100

    feat: started implementing the user profile page

--- components/ui/atom/cards/profile/ProfileNavCard.tsx
+++ components/ui/atom/cards/profile/ProfileNavCard.tsx
@@ -3,13 +3,24 @@ import React from "react";
 const ProfileNavCard = () => {
 	return (
 		<>
-			<div className="border overflow-x-auto flex xl:flex-col justify-between w-full items-start gap-4 bg-white/50 backdrop-blur-md">
-				<div className="p-4">sdfgh</div>
-				<div className="p-4">sdfgh</div>
-				<div className="p-4">sdfgh</div>
-				<div className="p-4">sdfgh</div>
-				<div className="p-4">sdfgh</div>
-				<div className="p-4">sdfgh</div>
+			<div className="border p-4 border-[#70C5A1] overflow-x-auto flex xl:flex-col justify-between w-full items-start gap-4 bg-white/50 backdrop-blur-md">
+				<div className="p-4 border border-[#70C5A1] w-full">sdfgh</div>
+
+				<div className="p-4 border border-[#70C5A1] bg-[#70C5A1] text-white w-full">
+					sdfgh
+				</div>
+				<div className="p-4 border border-[#70C5A1] bg-[#70C5A1] text-white w-full">
+					sdfgh
+				</div>
+				<div className="p-4 border border-[#70C5A1] bg-[#70C5A1] text-white w-full">
+					sdfgh
+				</div>
+				<div className="p-4 border border-[#70C5A1] bg-[#70C5A1] text-white w-full">
+					sdfgh
+				</div>
+				<div className="p-4 border border-[#70C5A1] bg-[#70C5A1] text-white w-full">
+					sdfgh
+				</div>
 			</div>
 		</>
 	);
