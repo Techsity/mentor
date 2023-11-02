3de8ef1dd5f1bf54514a519eb0040274346c41ca
Josh-Savvy <joshsavyy007@gmail.com>
2023-11-02 10:22:49 +0100

    feat: implementing the add course to wishlist feature - persist data from localstorage

--- redux/reducers/features/coursesSlice.ts
+++ redux/reducers/features/coursesSlice.ts
@@ -1,11 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { ICourse } from "../../../interfaces";
-import { slugify } from "../../../utils";
 import { RootState } from "../../store";
-import courses from "../../../data/courses";
+
+const isClient = typeof window !== "undefined" && window.localStorage;
+
+const userWishlistedCoursesLocal = isClient
+	? window.localStorage.getItem("userWishlistedCourses") || ""
+	: "";
+
+const data: ICourse[] = userWishlistedCoursesLocal
+	? JSON.parse(userWishlistedCoursesLocal)
+	: [];
 
 const initialState: { userWishlistedCourses: ICourse[] } = {
-	userWishlistedCourses: [],
+	userWishlistedCourses: data,
 };
 
 const coursesSlice = createSlice({
@@ -13,19 +21,16 @@ const coursesSlice = createSlice({
 	initialState,
 	reducers: {
 		setWishlist: (state, action: { payload: ICourse[] }) => {
-			// action.payload.forEach((course) => {
-			// 	if (
-			// 		!state.userWishlistedCourses.some(
-			// 			(existing) =>
-			// 				slugify(existing.title) === slugify(course.title),
-			// 		)
-			// 	) {
-			// 		state.userWishlistedCourses
-			// 	}
-
-			// });
-			state.userWishlistedCourses = action.payload;
-			// return state;
+			if (action.payload) {
+				state.userWishlistedCourses = action.payload;
+				// Save to local storage too, to persist state
+				if (isClient) {
+					window.localStorage.setItem(
+						"userWishlistedCourses",
+						JSON.stringify(action.payload),
+					);
+				}
+			}
 		},
 	},
 });
