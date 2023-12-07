40faab3552d5e5d2e3e42a59210b7af649c9753d
Josh-Savvy <joshsavyy007@gmail.com>
2023-12-07 13:00:42 +0100

    ui changes

--- pages/workshops/index.tsx
+++ pages/workshops/index.tsx
@@ -7,11 +7,9 @@ const AllWorkshopsPage = () => {
 	return (
 		<>
 			<h1 className="md:mx-10 m-6">All Workshops</h1>
-			<div className="grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 tracking-tight gap-6 overflow-hidden md:mx-10 mx-5 bg-[#FDFDFD] md:border border-[#D0D0D0] md:p-10 h-auto">
+			<div className="my-6 grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 tracking-tight gap-6 overflow-hidden md:mx-10 mx-5 bg-[#FDFDFD] md:border border-[#D0D0D0] md:p-10 h-auto">
 				{workshops.map((workshop: IWorkshop, indx: number) => {
-					return (
-						<WorkshopDisplayCard workshop={workshop} key={indx} />
-					);
+					return <WorkshopDisplayCard workshop={workshop} key={indx} />;
 				})}
 			</div>
 		</>
