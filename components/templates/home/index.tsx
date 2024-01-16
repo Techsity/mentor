031a6b7907c3c8fc3c03af5af143e355a0a5f30c
Josh-Savvy <joshsavyy007@gmail.com>
2024-01-16 10:28:12 +0100

    patch: fixed the view mentor page

--- components/templates/home/index.tsx
+++ components/templates/home/index.tsx
@@ -1,8 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import { CheckmarkSharp } from "react-ionicons";
-import { HomepageHeroSvg } from "../../ui/atom/icons/svgs";
-import Link from "next/link";
 import HomepageHero from "../../ui/organisms/home/hero";
 import HomepageTestimonialSection from "../../ui/organisms/home/testimonials";
 import Companies from "../../ui/organisms/home/companies";
