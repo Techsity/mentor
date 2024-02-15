45606650ffe1323e0b3a85c0bc67ea7b2859ac89
Josh-Savvy <joshsavyy007@gmail.com>
2024-02-15 19:59:23 +0100

    updated the logo to the beta version

--- components/ui/layout/navbar/index.tsx
+++ components/ui/layout/navbar/index.tsx
@@ -25,8 +25,9 @@ const Navbar = () => {
 
 	return (
 		<nav className="sticky h-20 top-0 w-full z-40 items-center bg-white shadow flex 2xl:gap-6 justify-between p-4 sm:px-12 tracking-tight oveflow-hidden animate__animated animate__fadeIn">
-			<div onClick={() => router.push("/")}>
+			<div onClick={() => router.push("/")} className="relative">
 				<MentorLogoDark className="cursor-pointer" />
+				<span className="absolute -right-5 -top-3 text-[#70C5A1] text-xs">Beta</span>
 			</div>
 			{!hideNavSections ? (
 				<div className="ml-6 xl:grid hidden flex-grow relative max-w-lg">
