59bbc0610d4dce117dae897206a41549b48d8b8b
Josh-Savvy <joshsavyy007@gmail.com>
2024-03-19 18:49:18 +0100

    fix build errors

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,15 +1,10 @@
-import React, { FC, useEffect, useMemo, useState } from "react";
+import React, { FC } from "react";
 import protectedPageWrapper from "../protectedPageWrapper";
 import ProfileLayout from "../../components/ui/layout/ProfileLayout";
 import { useSelector } from "react-redux";
-import CourseInProgressDisplayCard from "../../components/ui/atom/cards/course/CourseInProgressDisplayCard";
 import MentorProfileOverview from "../../components/ui/organisms/user/mentor/MentorProfileOverview.tsx";
-import { ICourse, ProfileTabLinkType } from "../../interfaces";
+import { ProfileTabLinkType } from "../../interfaces";
 import { currentUser } from "../../redux/reducers/authSlice";
-import courses from "../../data/courses";
-import { useQuery } from "@apollo/client";
-import { FETCH_COURSE_SUBSCRIPTIONS } from "../../services/graphql/mutations/user";
-import { Subscription } from "../../interfaces/user.interface";
 import UserCourseSubcriptions from "../../components/ui/organisms/user/profile/UserCourseSubcriptions";
 
 const UserProfilePage = () => {
