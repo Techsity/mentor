1da9183b06306c7f9480135412535e868cddba9b
Josh-Savvy <joshsavyy007@gmail.com>
2024-04-23 10:59:03 +0100

    updated the mentorship consultation page

--- components/templates/mentor/schedule-consultation/index.tsx
+++ components/templates/mentor/schedule-consultation/index.tsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useState } from "react";
 import MentorProfileCard from "../../../ui/atom/cards/mentor/MentorProfileCard";
 import { IMentor, IMentorAvailability, TimeSlot } from "../../../../interfaces/mentor.interface";
 import { daysOfTheWeek } from "../../../../constants";
 import classNames from "classnames";
 import { currentUser } from "../../../../redux/reducers/auth/authSlice";
 import { useSelector } from "react-redux";
-import { QueryClient, useQuery } from "@tanstack/react-query";
-import client from "../../../../utils/apolloClient";
-import { VIEW_MENTOR_AVAILABILITY, VIEW_MENTOR_PROFILE } from "../../../../services/graphql/queries/mentor";
-import { formatAmount, formatFollowersCount } from "../../../../utils";
 import { PrimaryButton } from "../../../ui/atom/buttons";
+import { CalendarOutline, TimeOutline } from "react-ionicons";
+import ActivityIndicator from "../../../ui/atom/loader/ActivityIndicator";
 
 type AvailabilitySubset = { id?: string; day: string; date: Date; timeSlots: TimeSlot[] };
 type SelectedAppointmentSlot = Omit<AvailabilitySubset, "timeSlots"> & { timeSlot: TimeSlot };
@@ -17,13 +15,24 @@ type SelectedAppointmentSlot = Omit<AvailabilitySubset, "timeSlots"> & { timeSlo
 const sortMentorAvailability = (availability: IMentorAvailability[]) => {
 	let availableDates: AvailabilitySubset[] = [];
 	if (availability) {
+		const currentDate = new Date();
 		for (const slot of availability) {
 			const { day, timeSlots, id } = slot;
-			const date = new Date();
+			let date = new Date();
 			const dayIndex = daysOfTheWeek.indexOf(day.toLowerCase());
-			date.setDate(date.getDate() + ((dayIndex - date.getDay() + 7) % 7));
+			date.setDate(date.getDate() + ((dayIndex - date.getDay()) % 7));
 			const availableSlot = { id, day, date, timeSlots: timeSlots.filter((slot) => slot.isOpen) };
 			availableDates.push(availableSlot);
+			if (date.getDay() < currentDate.getDay()) {
+				const newDateIndex = availableDates.findIndex((d) => d.date === date);
+				if (newDateIndex !== -1) {
+					// delete availableDates[newDateIndex];
+					availableDates.splice(newDateIndex, 1);
+					date.setDate(date.getDate() + ((dayIndex - date.getDay() + 7) % 7));
+					const slot = { id, day, date, timeSlots: timeSlots.filter((slot) => slot.isOpen) };
+					availableDates.push(slot);
+				}
+			}
 		}
 	}
 	return { availableDates };
@@ -45,7 +54,7 @@ const ScheduleConsultationTemplate = ({ loading, mentor }: { mentor?: IMentor; l
 	return (
 		<div className="py-10 h-full lg:px-20 sm:px-12 px-6 min-w-screen">
 			<MentorProfileCard mentor={mentor} detailsPage loading={loading} />
-			<div className="flex lg:flex-row flex-col justify-between gap-5 py-6 w-full md:mt-5 items-start animate__animated animate__fadeInUp overflow-hidden min-h-60">
+			<div className="lg:flex justify-between gap-5 py-6 w-full md:mt-5 items-start animate__animated animate__fadeInUp overflow-hidden lg:min-h-60">
 				<div className="bg-[#06310B] p-4 md:p-8 md:px-10 text-white lg:w-[35%] w-full">
 					<div className="flex justify-between items-center">
 						<h1 className="font-medium">Availability</h1>
@@ -70,19 +79,19 @@ const ScheduleConsultationTemplate = ({ loading, mentor }: { mentor?: IMentor; l
 						})}
 					</div>
 				</div>
-				<div className="flex-grow w-full lg:w-[35%]">
+				<div className="flex-grow w-full lg:w-[35%] mt-10 lg:mt-0">
 					<h1 className="text-[15px] font-medium">
-						Book a 1:1 mentorship session with{" "}
+						Select a convenient date and time for a 1:1 mentorship session with{" "}
 						<span className="capitalize">{`${mentor?.user.name.split(" ")[0]} ${
 							mentor?.user.name.split(" ")[1]
 						}`}</span>
 					</h1>
 					<div className="w-full">
 						<div className="w-full mt-4">
-							<p className="my-1">Available dates</p>
+							<p className="my-1 text-sm">Available dates</p>
 							<div className="flex items-center gap-3 w-full overflow-x-scroll hide-scroll-bar">
 								{availableDates.map((slot, index) => {
-									const { date, day, timeSlots } = slot;
+									let { date, day, timeSlots } = slot;
 									const remainingSlots = timeSlots.filter((slot) => slot.isOpen).length;
 									const month = date.toDateString().split(" ")[1];
 									return (
@@ -90,7 +99,7 @@ const ScheduleConsultationTemplate = ({ loading, mentor }: { mentor?: IMentor; l
 											key={index}
 											onClick={() => handleSelect(slot)}
 											className={classNames(
-												"select-none rounded border border-[#70C5A1] inline-block p-2 justify-center items-center h-20 w-full min-w-36 max-w-36 overflow-hidden cursor-pointer duration-300",
+												"select-none rounded border border-[#70C5A1] inline-flex p-2 justify-between items-start h-20 w-full min-w-36 max-w-36 overflow-hidden cursor-pointer duration-300",
 												selectedAvailability?.id === slot.id
 													? "bg-[#70C5A1] text-white"
 													: "hover:bg-[#70C5A1]/10",
@@ -107,13 +116,18 @@ const ScheduleConsultationTemplate = ({ loading, mentor }: { mentor?: IMentor; l
 													{/* available */}
 												</span>
 											</div>
+											<CalendarOutline
+												color={selectedAvailability?.id === slot.id ? "#fff" : "#000"}
+												width={"18px"}
+												height="18px"
+											/>
 										</div>
 									);
 								})}
 							</div>
 							{selectedAvailability && (
 								<div className="w-full mt-4">
-									<p className="my-1">Available Slots</p>
+									<p className="my-1 text-sm">Available Slots</p>
 									<div className="flex snap-x duration-300 snap-mandatory items-center gap-3 w-full overflow-x-scroll hide-scroll-bar">
 										{selectedAvailability.timeSlots.length >= 1 ? (
 											selectedAvailability.timeSlots
@@ -128,6 +142,9 @@ const ScheduleConsultationTemplate = ({ loading, mentor }: { mentor?: IMentor; l
 												})
 												.map((slot, index) => {
 													const { endTime, startTime } = slot;
+													const isTime =
+														selectedAppointmentSlot?.timeSlot?.endTime === endTime &&
+														selectedAppointmentSlot?.timeSlot?.startTime === startTime;
 													return (
 														<div
 															onClick={() =>
@@ -138,17 +155,17 @@ const ScheduleConsultationTemplate = ({ loading, mentor }: { mentor?: IMentor; l
 															}
 															key={index}
 															className={classNames(
-																"select-none rounded border inline-flex justify-center items-center p-2 min-w-28 w-full max-w-28 overflow-hidden cursor-pointer duration-300 text-sm",
-																selectedAppointmentSlot?.timeSlot?.endTime ===
-																	endTime &&
-																	selectedAppointmentSlot?.timeSlot?.startTime ===
-																		startTime
+																"select-none rounded border inline-flex justify-center gap-2 items-center p-2 min-w-28 w-full max-w-28 overflow-hidden cursor-pointer duration-300 text-sm",
+																isTime
 																	? "bg-[#FFB100] border-[#FFB100] text-[#06310B]"
 																	: "hover:bg-[#FFB100]/10 border-[#06310B]",
 															)}>
+															<TimeOutline
+																color={isTime ? "#06310B" : "#000"}
+																width={"16px"}
+																height="16px"
+															/>
 															<span>{startTime}</span>
-															{/* <span>-</span>
-															<span>{endTime}</span> */}
 														</div>
 													);
 												})
@@ -158,17 +175,20 @@ const ScheduleConsultationTemplate = ({ loading, mentor }: { mentor?: IMentor; l
 											</>
 										)}
 									</div>
-									{selectedAppointmentSlot !== null && (
-										<PrimaryButton
-											title="Set appointment"
-											className="my-5 p-2 text-sm rounded px-5 flex justify-center items-center animate__animated animate__fadeIn animate__fast"
-										/>
-									)}
+									{/* { && ( */}
+									<PrimaryButton
+										// title="Continue"
+										title="Book a free trial"
+										disabled={selectedAppointmentSlot === null}
+										className="my-5 p-2.5 text-sm rounded px-5 flex justify-center items-center animate__animated animate__fadeIn animate__fast"
+									/>
+									{/* )} */}
 								</div>
 							)}
 						</div>
 					</div>
 					<br />
+					<ActivityIndicator />
 					<p className="max-w-sm text-[15px]">
 						Schedules are inconvenient for you?
 						<br />
